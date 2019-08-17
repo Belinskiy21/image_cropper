8e9a0ac673a61f41aa5c8f549a8333358dd58be4
Oleg <belinskiy21@gmail.com>
2019-08-17 22:26:20 +0300

    add error tests for invalid params when sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
-  
+
   def create
-    user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
-    json_response(response, :created)
+    user = User.new(user_params)
+    if user.save
+      auth_token = AuthenticateUser.new(user.email, user.password).call
+      response = { message: Message.account_created, auth_token: auth_token }
+      json_response(response, :created)
+    else
+      response = { message: user.errors.full_messages }
+      json_response(response, 422)
+    end
   end
 
   private
